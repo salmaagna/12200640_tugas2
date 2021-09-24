6c885367ff23bf0921afa5ecde4fe501329a137b
salmaagna <agnasalma9@gmail.com>
2021-09-24 06:49:07 -0700

    menambahkan framework

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"nama saya Salma Agna
+             saya tinggal di daerah purbaratu
+             olah raga yang aku sukai adalah 
+             renang
+             no hp 087794534027";
+    }
+}
+?>
